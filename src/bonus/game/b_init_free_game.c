61af62b8f959814ef1a668dd938b8709c48e624e
Ali Aslan <aliaslan_61@outlook.com>
2023-11-11 12:17:26 +0300

    imageleri null başlattık

--- src/bonus/game/b_init_free_game.c
+++ src/bonus/game/b_init_free_game.c
@@ -6,11 +6,30 @@
 /*   By: aaslan <aaslan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 23:24:22 by aaslan            #+#    #+#             */
-/*   Updated: 2023/11/09 10:53:18 by aaslan           ###   ########.fr       */
+/*   Updated: 2023/11/11 12:14:56 by aaslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../b_cub3d.h"
+
+static void	b_init_game_null(t_cub3d *cub3d)
+{
+	cub3d->game->mlx = NULL;
+	cub3d->game->window = NULL;
+	cub3d->game->screen = NULL;
+	cub3d->game->player = NULL;
+	cub3d->game->north_image = NULL;
+	cub3d->game->south_image = NULL;
+	cub3d->game->west_image = NULL;
+	cub3d->game->east_image = NULL;
+	cub3d->game->ceiling_rgb = 0;
+	cub3d->game->floor_rgb = 0;
+	cub3d->game->door_image = NULL;
+	cub3d->game->door0_image = NULL;
+	cub3d->game->door1_image = NULL;
+	cub3d->game->door2_image = NULL;
+	cub3d->game->door3_image = NULL;
+}
 
 static void	b_init_keys(t_cub3d *cub3d)
 {
@@ -38,12 +57,7 @@ void	b_init_game(t_cub3d *cub3d)
 	cub3d->game = malloc(sizeof(t_game));
 	if (cub3d->game == NULL)
 		b_print_error(cub3d, "game malloc error.");
-	cub3d->game->mlx = NULL;
-	cub3d->game->window = NULL;
-	cub3d->game->screen = NULL;
-	cub3d->game->player = NULL;
-	cub3d->game->ceiling_rgb = 0;
-	cub3d->game->floor_rgb = 0;
+	b_init_game_null(cub3d);
 	b_init_mlx(cub3d);
 	b_init_screen(cub3d);
 	b_init_north_mlx_image(cub3d);
--- src/mandatory/game/init_free_game.c
+++ src/mandatory/game/init_free_game.c
@@ -6,11 +6,25 @@
 /*   By: aaslan <aaslan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 23:24:22 by aaslan            #+#    #+#             */
-/*   Updated: 2023/11/09 03:47:24 by aaslan           ###   ########.fr       */
+/*   Updated: 2023/11/11 12:12:24 by aaslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+static void	init_game_null(t_cub3d *cub3d)
+{
+	cub3d->game->mlx = NULL;
+	cub3d->game->window = NULL;
+	cub3d->game->screen = NULL;
+	cub3d->game->player = NULL;
+	cub3d->game->north_image = NULL;
+	cub3d->game->south_image = NULL;
+	cub3d->game->west_image = NULL;
+	cub3d->game->east_image = NULL;
+	cub3d->game->ceiling_rgb = 0;
+	cub3d->game->floor_rgb = 0;
+}
 
 static void	init_keys(t_cub3d *cub3d)
 {
@@ -27,12 +41,7 @@ void	init_game(t_cub3d *cub3d)
 	cub3d->game = malloc(sizeof(t_game));
 	if (cub3d->game == NULL)
 		print_error(cub3d, "game malloc error.");
-	cub3d->game->mlx = NULL;
-	cub3d->game->window = NULL;
-	cub3d->game->screen = NULL;
-	cub3d->game->player = NULL;
-	cub3d->game->ceiling_rgb = 0;
-	cub3d->game->floor_rgb = 0;
+	init_game_null(cub3d);
 	init_mlx(cub3d);
 	init_screen(cub3d);
 	init_north_mlx_image(cub3d);
